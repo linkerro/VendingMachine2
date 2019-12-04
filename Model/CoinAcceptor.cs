b37beb82bfd69f3e6a172164b0226fe6eb2547b5
Radu Iscu <linekerro@gmail.com>
2019-12-04 18:08:33 +0200

    Added Coin Acceptor

---
+++ Model/CoinAcceptor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public class CoinAcceptor
+    {
+        private List<decimal> _acceptedCoins;
+        private decimal _total;
+
+        public List<decimal> AcceptedCoins => _acceptedCoins;
+        public decimal Total => _total;
+
+        public event EventHandler<decimal> CoinInserted;
+
+        public void Setup(decimal coin)
+        {
+            if (AcceptedCoins == null)
+            {
+                _acceptedCoins = new List<decimal>();
+            }
+            _acceptedCoins.Add(coin);
+        }
+
+        public void Insert(decimal v)
+        {
+            _total += v;
+            CoinInserted?.Invoke(this, v);
+        }
+
+        public decimal Empty()
+        {
+            var total = _total;
+            _total = 0;
+            return total;
+        }
+    }
+}
---
+++ ModelSpecs/CoinAcceptorSpecs.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+
+namespace ModelSpecs
+{
+    [TestClass]
+    public class CoinAcceptorSpecs
+    {
+        [TestMethod]
+        public void IsSetToAcceptCoins()
+        {
+            var coinAcceptor = new CoinAcceptor();
+            coinAcceptor.Setup(.05m);
+
+            CollectionAssert.AreEqual(new decimal[] { .05m }, coinAcceptor.AcceptedCoins);
+        }
+        [TestMethod]
+        public void IsAcceptingCoins()
+        {
+            var coinAcceptor = new CoinAcceptor();
+            coinAcceptor.Setup(.05m);
+
+            coinAcceptor.Insert(.05m);
+        }
+        [TestMethod]
+        public void IsNotifyingOfInsertedCoins()
+        {
+            var coinAcceptor = new CoinAcceptor();
+            coinAcceptor.Setup(.05m);
+
+            var insertedCoin = 0m;
+            coinAcceptor.CoinInserted += (sender,coinValue) => insertedCoin = coinValue;
+
+            coinAcceptor.Insert(.05m);
+
+            Assert.AreEqual(.05m, insertedCoin);
+        }
+        [TestMethod]
+        public void IsCalculatingTotal()
+        {
+            var coinAcceptor = new CoinAcceptor();
+            coinAcceptor.Setup(.05m);
+            coinAcceptor.Setup(.03m);
+
+            coinAcceptor.Insert(.05m);
+            coinAcceptor.Insert(.05m);
+            coinAcceptor.Insert(.03m);
+
+            Assert.AreEqual(0.13m, coinAcceptor.Total);
+        }
+        [TestMethod]
+        public void IsEmptying()
+        {
+            var coinAcceptor = new CoinAcceptor();
+            coinAcceptor.Setup(.05m);
+            coinAcceptor.Setup(.03m);
+
+            coinAcceptor.Insert(.05m);
+            coinAcceptor.Insert(.05m);
+            coinAcceptor.Insert(.03m);
+            
+            coinAcceptor.Empty();
+
+            Assert.AreEqual(0, coinAcceptor.Total);
+        }
+        [TestMethod]
+        public void IsReturningTotalWhenEmptying()
+        {
+            var coinAcceptor = new CoinAcceptor();
+            coinAcceptor.Setup(.05m);
+            coinAcceptor.Setup(.03m);
+
+            coinAcceptor.Insert(.05m);
+            coinAcceptor.Insert(.05m);
+            coinAcceptor.Insert(.03m);
+
+            var total= coinAcceptor.Empty();
+
+            Assert.AreEqual(.13m, total);
+        }
+    }
+}
